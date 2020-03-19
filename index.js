8772a3ef46dc34341d387d31735c79675f6e923f
unknown <hereiskeith@gmail.com>
2020-03-19 09:36:48 -0400

    setup Google OAuth

--- index.js
+++ index.js
@@ -3,16 +3,34 @@ const express = require('express');
 // commonJS implemented in node.js for requiring and sharing code between different files
 // node.js doesn't have support for ES2015 module
 
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
 const app = express();
 // Define a new application that represents a running express app
 
-// Create a route handler
-app.get('/', (req, res) => {
-  // '/': watch for requests trying to access '/'
-  res.send({ bye: 'buddy' });
-  // Tell the express that we want to immediately
-  //close the request and send back response containing the JSON data
-});
+// console.developers.google.com
+passport.use(new GoogleStrategy(
+  {
+  clientID: keys.googleClientID,
+  clientSecret: keys.googleClientSecret,
+  callbackURL: '/auth/google/callback'
+  },
+  (accessToken, refreshToken, profile, done) => {
+    console.log(accessToken, refreshToken, profile);
+  }
+  )
+);
+
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+  scope: ['profile', 'email']
+  })
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 
 // Whenever Heroku runs on app, it has the ability to
@@ -24,3 +42,18 @@ const PORT = process.env.PORT || 5000;
 // declare a port for us to use
 app.listen(PORT);
 // Tell node.js to listen to port 5000
+
+
+
+
+
+
+
+
+// Create a route handler
+// app.get('/', (req, res) => {
+//   // '/': watch for requests trying to access '/'
+//   res.send({ bye: 'buddy' });
+//   // Tell the express that we want to immediately
+//   //close the request and send back response containing the JSON data
+// });
---
+++ notes/Google OAuth.md
@@ -0,0 +1,12 @@
+##Passport library components
+####`npm install --save passport passport-google-oauth20`
+
+### 1. passport 
+General helpers for handling auth in Express apps
+
+### 2. passport strategy
+Helpers for authenticating with one very specific method (email/password, Google, Facebook, etc)
+
+##Nodemon
+A tool to restart index.js every time we make any changes so we don't have to type node index.js again and again.
+####`npm install --save nodemon`
